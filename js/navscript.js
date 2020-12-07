c4ac3f224b38b7678491deb1582553f8417abbd5
Javier Diaz <diazjavierdavid@hotmail.com>
2020-12-06 23:41:27 -0300

    cambio js nav

--- js/navscript.js
+++ js/navscript.js
@@ -1,3 +1,17 @@
+// Shrink nav
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    document.getElementById("navbar").style.padding = "10px 10px";
+    document.getElementById("navbar").style.backgroundColor = "var(--clr--bgc1-footer)";
+  } else {
+    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+  }
+}
+
 // Toggle button
 
 const navToggle = document.querySelector('.toggle-button');
@@ -12,17 +26,3 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav__list-open');
     })
 });
-
-// Shrink nav
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    document.getElementById("navbar").style.padding = "10px 10px";
-    document.getElementById("navbar").style.backgroundColor = "var(--clr--bgc1-footer)";
-  } else {
-    document.getElementById("navbar").style.padding = "20px 10px";
-    document.getElementById("navbar").style.backgroundColor = "transparent";
-  }
-}
